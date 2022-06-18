57be93d2611e3b15dc94c0c3a7f7ba41ccd1baa4
sauln179 <saulnavarro179@gmail.com>
2022-06-18 00:53:25 -0700

    editing

--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import ProjectCards from './Project'
-import portfolio from '../portfolio.json'
+import portfolio from '../../portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Wrapper(props) {
